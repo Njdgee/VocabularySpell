9eb28ebdae4f76826a3a7b82ce623c668a90192b
RTX <jaykuo1203@gmail.com>
2024-05-06 22:28:33 +0800

    我不會嗚嗚嗚嗚嗚嗚嗚 幹你娘又被gank我要去讀書了拉幹

---
+++ src/main/java/com/njdge/VocabularyGUI.java
@@ -0,0 +1,9 @@
+package com.njdge;
+
+import com.njdge.gui.VocFrame;
+
+public class VocabularyGUI {
+    public static void main(String[] args) {
+        VocFrame vocFrame = new VocFrame();
+    }
+}
